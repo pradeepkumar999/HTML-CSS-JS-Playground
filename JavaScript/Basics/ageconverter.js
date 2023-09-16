269eb4bf30d54c665b3bc73b05bb160d6865ac4a
pradeep <pradeepvirat96@gmail.com>
2023-09-16 12:16:35 +0530

    basic js programmes

---
+++ JavaScript/Basics/ageconverter.js
@@ -0,0 +1,7 @@
+function ageconert(age){
+    var years=90-age;
+    var weeks=years*52;
+    var days=weeks*7;
+  console.log("your ramain with years of "+years+" weeks "+weeks+" days "+days);
+  }
+  ageconert(21);
