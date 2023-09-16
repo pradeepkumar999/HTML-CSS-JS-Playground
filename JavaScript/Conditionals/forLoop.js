3806e756bc1206f86a24b0fe90027727b6a452c1
pradeep <pradeepvirat96@gmail.com>
2023-09-16 14:41:04 +0530

    1-100 using for loop

---
+++ JavaScript/Conditionals/forLoop.js
@@ -0,0 +1,17 @@
+/*
+1-100 using for loop
+*/
+
+var arr=[];
+function number(count){
+  for(var count=1;count<=100;count++){
+    arr.push(count);
+     
+  }
+  return arr;
+}
+
+var print=number(count);
+
+console.log(print);
+
