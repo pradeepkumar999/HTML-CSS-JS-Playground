94e9da19077012ad60ca20cdde4d6d8189c2c2b5
pradeep <pradeepvirat96@gmail.com>
2023-09-16 12:20:09 +0530

    love calculator

---
+++ JavaScript/Basics/loveCaluclator.js
@@ -0,0 +1,7 @@
+var youName=prompt("enter you name");
+var yourLove=prompt("enter your love name");
+
+var n=Math.random();
+      n=n*6;
+      n=Math.floor(n)+1;
+alert("your love percentage is "+n);
