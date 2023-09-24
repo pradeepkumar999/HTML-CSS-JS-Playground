ad7ef3643338da41e6bca00cf11d11cfd2dbc6f0
pradeep <pradeepvirat96@gmail.com>
2023-09-24 14:27:05 +0530

    adding animation

--- JavaScript/DrumKit/index.js
+++ JavaScript/DrumKit/index.js
@@ -1,10 +1,24 @@
+//detecting button press
 var numberOfDrumButtons=document.querySelectorAll(".drum").length;
 for(var i=0;i<numberOfDrumButtons;i++) {
 
 document.querySelectorAll(".drum")[i].addEventListener("click",function(){
     var buttonInnerHTML=this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+    
+}
 
-    switch(buttonInnerHTML){
+);
+}
+//detecting keyboard press
+document.addEventListener("keypress",function(event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+    });
+
+    function makeSound(key){
+    switch(key){
         case "w":
             var crash=new Audio("sounds/carsh.mp3");
             crash.play();
@@ -37,6 +51,12 @@ document.querySelectorAll(".drum")[i].addEventListener("click",function(){
         default: console.log(buttonInnerHTML);
 
     }
-
-});
 }
+function buttonAnimation(currentKey){
+    var activeButton=document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed"); 
+    },100);
+    }
+
